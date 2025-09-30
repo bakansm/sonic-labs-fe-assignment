b473faf7fe0c41a7480e88fee6b401b3dfbd6d35
bakansm <khuynhbao5@gmail.com>
2025-09-30 19:00:42 +0700

    feat: implement header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,23 @@
+import { GlobeIcon, MenuIcon } from 'lucide-react';
+import { Button } from '../ui/button';
+
 export const Header = () => {
-  return <header></header>;
+  return (
+    <header className='fixed top-0 left-0 z-50 flex h-16 w-full items-center justify-center bg-background'>
+      <div className='mx-auto flex w-full max-w-7xl items-center justify-between px-2.5'>
+        <h1 className='text-body-lg'>Logo</h1>
+        <div className='flex h-min items-center gap-x-2.5'>
+          <Button shape={'circular'} variant={'gradient'}>
+            Connect Wallet
+          </Button>
+          <Button className='hidden sm:block' icon shape={'circular'} variant={'gradient'}>
+            <GlobeIcon />
+          </Button>
+          <Button className='block sm:hidden' color={'dark'} icon shape={'circular'} variant={'outlined'}>
+            <MenuIcon />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
